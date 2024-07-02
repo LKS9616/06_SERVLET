ad2f0d8b7d6e3f3fde9bebcd2fd4af1105d23a6a
lks9616 <lks609591@gmail.com>
2024-07-02 18:05:40 +0900

    chap04-chap08

---
+++ chap05-response-lecture-source/src/main/java/com/ohgiraffers/section03/status/Status.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.section03.status;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+
+@WebServlet("/status")
+public class Status extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 200, 400, 401, 404, 500
+
+        response.sendError(200, "요청이 성공적으로 진행되었습니다.");
+
+        response.sendError(400, "잘못된 문법으로 인해 서버가 요청을 이해할 수 없습니다.");
+
+        response.sendError(401, "클라이언트가 현재 미인증 상태입니다");
+
+        response.sendError(404, "없는 페이지입니다. 경로를 확인해주세요");
+
+        response.sendError(500, "서버 내부 오류입니다. 서버 오류는 개발자의 잘못이고, 개발자는 여러분입니다!@");
+
+    }
+
+}
---
+++ chap08-redirect-lecture-source/src/main/java/com/ohgiraffers/section01/othersite/OtherSiteRedirectSerblet.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section01.othersite;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+
+@WebServlet("/")
+public class OtherSiteRedirectSerblet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("get 요청 후 naver 사이트로 redirect");
+
+        response.sendRedirect("http://naver.com");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
